d04ec94add0c2871efca6a836f999713a7c97aa9
00125495 <rajendra@mail-me.com>
2020-06-14 15:02:08 +0800

    ver4

---
+++ stringiteration.py
@@ -0,0 +1,7 @@
+
+count = 0
+for letter in 'Hello world':
+    if(letter == 'l'):
+        count +=1
+
+print(count, 'letters found')
---
+++ tuple.py
@@ -0,0 +1,22 @@
+#tuple - immutable ordered finite sequnce type
+#list - mutable ordered sqence type
+# Create tuple - with single element
+# tuple packing, unpacking
+# Accessing using indexing, negitive indexing
+# slicing [start:end:step]
+# update the element in tuple
+# update element in sublist in tuple
+# deleting the tuple
+# tuple methods - index, count
+# tuple membership test - in, not in 
+# iterate through tuple
+# list vs tuple
+#   1. sysntax difference 
+#   2. mutable vs immutable
+#   3. dir() - list of menthods 
+#   4. size comparision a.__sizeof__()
+#   5. function vs method
+#   6. tuples are similar to dicts without keys
+#   7. Tuple can be used as key for dict where as 
+#       we cannot list
+#   8. hashing - tuple can be hasble whereas list cannot
